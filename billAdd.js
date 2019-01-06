b06757a8b773b41f6d783cbf3b35bf331add1cb0
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-01-06 14:00:55 +0000

    tweaked algortihm to accept a dynamic tip percentage amount

--- billAdd.js
+++ billAdd.js
@@ -9,19 +9,19 @@ function billAdd(item1, item2, item3) {
 
 console.log(billAdd(20.52, 9.27, 11.99));
 
-// - hacker level add 15% tip and accept an array of undetermined amount of items
+// - hacker level add a dynamic tip amount and accept an array of undetermined amount of items
 
-function tipEm(arr) {
+function tipEm(arr, tip) {
     var total = 0;
     for (var i=0; i<arr.length; i++) {
         total += arr[i];
     }
     var tax = total * .07;
-    var tip = total * .15;
+    var tip = tip * .01;
+    tip = total * tip;
     var withTip = tax + tip + total;
     return withTip.toFixed(2);
 }
 
-console.log(tipEm([20.33, 9.27, 11.99]));
-
+console.log(tipEm([20.33, 9.27, 11.99], 20));
 
