3cb06099209a9573d2563e5a2480c19edd096a3e
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-01-06 13:53:54 +0000

    used toFixed(2) to account for returns with more than two decimal places

--- billAdd.js
+++ billAdd.js
@@ -4,12 +4,12 @@ function billAdd(item1, item2, item3) {
     var total = item1 + item2 + item3;
     var tax = total * .07;
     var totalTax = total + tax;
-    return totalTax;
-};
+    return totalTax.toFixed(2);
+}
 
-console.log(billAdd(20, 9, 11));
+console.log(billAdd(20.52, 9.27, 11.99));
 
-// - hacker level add 15% tip
+// - hacker level add 15% tip and accept an array of undetermined amount of items
 
 function tipEm(arr) {
     var total = 0;
@@ -19,9 +19,9 @@ function tipEm(arr) {
     var tax = total * .07;
     var tip = total * .15;
     var withTip = tax + tip + total;
-    return withTip;
-};
+    return withTip.toFixed(2);
+}
 
-console.log(tipEm([20, 9, 11]));
+console.log(tipEm([20.33, 9.27, 11.99]));
 
 
